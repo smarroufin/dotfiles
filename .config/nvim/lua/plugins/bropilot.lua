702bf4d04e8e9019a4058d405d695b664e340c4b
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2026-03-01 16:02:08 +0100

    chore(nvim): bropilot

--- .config/nvim/lua/plugins/bropilot.lua
+++ .config/nvim/lua/plugins/bropilot.lua
@@ -1,16 +1,22 @@
 return {
   {
     'meeehdi-dev/bropilot.nvim',
-    enabled = false,
     event = 'VeryLazy',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'j-hui/fidget.nvim',
+      'Leonardo-Luz/dotenv.nvim',
     },
+    config = function(_, opts)
+      require('dotenv').load()
+      opts.api_key = vim.env.MISTRAL_CODESTRAL_API_KEY
+      require('bropilot').setup(opts)
+    end,
     opts = {
-      model = 'qwen2.5-coder:7b-base',
+      -- model = 'qwen2.5-coder:7b-base',
       -- model = 'starcoder2:7b',
       -- model = 'devstral:latest',
+      provider = 'codestral',
       auto_suggest = false,
       keymap = {
         accept_word = '<C-l>',
