341e71e57abece725f63c05fea5a581e3cc67cef
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2026-03-05 23:28:44 +0100

    chore(nvim): bropilot update

--- .config/nvim/lua/plugins/bropilot.lua
+++ .config/nvim/lua/plugins/bropilot.lua
@@ -9,21 +9,46 @@ return {
     },
     config = function(_, opts)
       require('dotenv').load()
-      opts.api_key = vim.env.MISTRAL_CODESTRAL_API_KEY
+      local providers = {
+        mistralEnterprise = {
+          provider = 'mistral',
+          api_key = vim.env.MISTRAL_ENTERPRISE_API_KEY,
+        },
+        mistralOpensource = {
+          provider = 'mistral',
+          api_key = vim.env.MISTRAL_OPENSOURCE_API_KEY,
+        },
+        codestral = {
+          provider = 'codestral',
+          api_key = vim.env.MISTRAL_CODESTRAL_API_KEY,
+        },
+      }
+      local privateFolders = vim.split(vim.env.PRIVATE_FOLDERS or '', ',')
+      local isPrivateFolder = false
+      local cwd = vim.fn.getcwd()
+      for _, folder in ipairs(privateFolders) do
+        local start = string.find(cwd, folder, 1, true)
+        if start ~= nil then
+          isPrivateFolder = true
+          break
+        end
+      end
+      if isPrivateFolder then
+        vim.tbl_extend('force', opts, providers.mistralEnterprise)
+      else
+        vim.tbl_extend('force', opts, providers.mistralOpensource)
+      end
       require('bropilot').setup(opts)
     end,
     opts = {
       -- model = 'qwen2.5-coder:7b-base',
       -- model = 'starcoder2:7b',
-      -- model = 'devstral:latest',
-      provider = 'codestral',
       auto_suggest = false,
       keymap = {
         accept_word = '<C-l>',
-        accept_line = '<C-M-l>',
+        accept_line = '<S-Tab>',
         accept_block = '<Tab>',
         suggest = '<C-j>',
-        suggest_next = '<C-Down>',
       },
     },
   },
