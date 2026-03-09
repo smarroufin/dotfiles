7c5ab978482df4c6eb6a09148861200f30dac79b
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2026-03-09 23:03:42 +0100

    chore(nvim): update gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,14 +2,51 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+      current_line_blame = true,
+    },
     keys = {
       {
-        '<leader>ga',
+        '<leader>gb',
         function()
           require('gitsigns.actions').blame_line()
         end,
         desc = 'Git blame',
+      },
+      {
+        '<leader>gp',
+        function()
+          require('gitsigns.actions').preview_hunk()
+        end,
+        desc = 'Preview hunk',
+      },
+      {
+        '<leader>gs',
+        function()
+          require('gitsigns.actions').stage_hunk()
+        end,
+        desc = 'Toggle hunk',
+      },
+      {
+        '<leader>gR',
+        function()
+          require('gitsigns.actions').reset_hunk()
+        end,
+        desc = 'Reset hunk',
+      },
+      {
+        '<leader>gk',
+        function()
+          require('gitsigns.actions').nav_hunk('prev')
+        end,
+        desc = 'Previous hunk',
+      },
+      {
+        '<leader>gj',
+        function()
+          require('gitsigns.actions').nav_hunk('next')
+        end,
+        desc = 'Next hunk',
       },
     },
   },
