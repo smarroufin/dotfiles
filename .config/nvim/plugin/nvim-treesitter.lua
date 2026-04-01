b1464086354b089ae1f6c2201fd885045cb42400
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2026-04-01 23:31:11 +0200

    chore: treesitter update

--- .config/nvim/plugin/nvim-treesitter.lua
+++ .config/nvim/plugin/nvim-treesitter.lua
@@ -1,15 +1,12 @@
 vim.pack.add({
-  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/nvim-treesitter/nvim-treesitter', -- requires tree-sitter-cli
   'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
   'https://github.com/nvim-treesitter/nvim-treesitter-context',
 })
 vim.api.nvim_create_autocmd('PackChanged', {
   callback = function(ev)
     local name, kind = ev.data.spec.name, ev.data.kind
-    if name == 'nvim-treesitter' and kind == 'update' then
-      if not ev.data.active then
-        vim.cmd.packadd('nvim-treesitter')
-      end
+    if name == 'nvim-treesitter' and (kind == 'install' or kind == 'update') then
       vim.cmd('TSUpdate')
     end
   end,
@@ -35,16 +32,8 @@ local ts_parsers = {
   'yaml',
 }
 
-require('nvim-treesitter').setup({
-  ensure_installed = ts_parsers,
-  sync_install = false,
-  auto_install = true,
-  highlight = { enable = true },
-  indent = { enable = true },
-})
--- require("nvim-treesitter").install(ts_parsers)
+require('nvim-treesitter').install(ts_parsers)
 require('treesitter-context').setup({
-  mode = 'cursor',
   max_lines = 3,
   multiline_threshold = 1,
 })
