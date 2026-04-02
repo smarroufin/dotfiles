f63512007752a3dd762ff3d5efe593290091f82b
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2026-04-02 20:59:49 +0200

    fix: force TS highlight

--- .config/nvim/plugin/nvim-treesitter.lua
+++ .config/nvim/plugin/nvim-treesitter.lua
@@ -6,6 +6,24 @@ vim.api.nvim_create_autocmd('PackChanged', {
     end
   end,
 })
+-- https://github.com/nvim-treesitter/nvim-treesitter/blob/6878ae017d691a0df1c49d8a091c88a33e4c2d50/doc/nvim-treesitter.txt#L39-L52
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '*' },
+  callback = function(ev)
+    local ft = vim.bo[ev.buf].filetype
+    local lang = vim.treesitter.language.get_lang(ft)
+    if not lang then
+      return
+    end
+    if not vim.treesitter.language.add(lang) then
+      return
+    end
+    -- syntax highlighting, provided by Neovim
+    vim.treesitter.start(ev.buf, lang)
+    -- indentation, provided by nvim-treesitter
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
 
 vim.pack.add({
   'https://github.com/nvim-treesitter/nvim-treesitter', -- requires tree-sitter-cli
